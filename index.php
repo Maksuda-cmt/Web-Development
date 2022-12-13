b666b3e52f4fdbd9b869f6c6f502701cb7058dca
Maksuda-cmt <116251768+Maksuda-cmt@users.noreply.github.com>
2022-12-13 14:08:43 +0600

    Add files via upload

--- index.php
+++ index.php
@@ -2,8 +2,46 @@
 <html>
 <body>
 
+
+<h1>My first PHP page</h1>
+
 <?php
-echo "My first PHP script!";
+// variable
+/*
+malti line comment
+*/
+$color = "Maksuda Akter";
+echo $color . "<br>";
+
+$txt = "text";
+echo "this is $txt". "<br>";
+// variable concat
+$txt2 = "this is text two,";
+echo "this is text one," . $txt2 . "this is text three". "<br>";
+
+// variable addition
+$x = 5;
+$y = 10;
+echo $x + $y;
+
+// array
+$cars =array("BW","VOLVO","TOYOTA");
+var_dump($cars);
+
+// class
+class car {
+    public $model = "MERCEDES";
+    public $color = "BLACK";
+
+    public function getCarInformation(){
+        return "model : " . $this->model . "color : " .$this->color;
+    }
+}
+$car1 = new car();
+
+echo $car1->getCarInformation();
+
+
 ?>
 
 </body>
